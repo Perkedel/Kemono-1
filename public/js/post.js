266d061b1db93e115503acd1da51c5090c7f162f
Anonymous <>
2020-06-29 14:11:54 -0400

    Non-blocking cache loading

--- public/js/post.js
+++ public/js/post.js
@@ -11,56 +11,56 @@ function attemptFlag (e, api) {
 async function main () {
   const pathname = window.location.pathname.split('/');
   const resultsView = document.getElementById('results');
-  let posts, cache, flagApi;
+  let posts, cacheApi, flagApi;
   switch (document.getElementsByName('service')[0].content) {
     case 'patreon': {
       const postData = await fetch(`/api/user/${pathname[2]}/post/${pathname[4]}`);
       posts = await postData.json();
-      const cacheData = await fetch(`/api/lookup/cache/${pathname[2]}?service=patreon`);
-      cache = await cacheData.json();
+      cacheApi = `/api/lookup/cache/${pathname[2]}?service=patreon`;
       flagApi = `/api/user/${pathname[2]}/post/${pathname[4]}/flag`;
       break;
     }
     case 'gumroad': {
       const postData = await fetch(`/api/gumroad/user/${pathname[3]}/post/${pathname[5]}`);
       posts = await postData.json();
-      const cacheData = await fetch(`/api/lookup/cache/${pathname[3]}?service=gumroad`);
-      cache = await cacheData.json();
+      cacheApi = `/api/lookup/cache/${pathname[3]}?service=gumroad`;
       flagApi = `/api/gumroad/user/${pathname[3]}/post/${pathname[5]}/flag`;
       break;
     }
     case 'subscribestar': {
       const postData = await fetch(`/api/subscribestar/user/${pathname[3]}/post/${pathname[5]}`);
       posts = await postData.json();
-      const cacheData = await fetch(`/api/lookup/cache/${pathname[3]}?service=subscribestar`);
-      cache = await cacheData.json();
+      cacheApi = `/api/lookup/cache/${pathname[3]}?service=subscribestar`;
       flagApi = `/api/subscribestar/user/${pathname[3]}/post/${pathname[5]}/flag`;
       break;
     }
     default: {
       const postData = await fetch(`/api/fanbox/user/${pathname[3]}/post/${pathname[5]}`);
       posts = await postData.json();
-      const cacheData = await fetch(`/api/lookup/cache/${pathname[3]}?service=fanbox`);
-      cache = await cacheData.json();
+      cacheApi = `/api/lookup/cache/${pathname[3]}?service=fanbox`;
       flagApi = `/api/fanbox/user/${pathname[3]}/post/${pathname[5]}/flag`;
     }
   }
 
-  resultsView.innerHTML += `
-    <li>
-      User: <a href="../">${cache.name}</a>
-    </li>
-    <li>
-      ID: <a href="">${posts[0].id}</a>
-    </li>
-    <li>
-      Published at: ${posts[0].published_at}
-    </li>
-    <li>
-      Added at: ${new Date(posts[0].added_at).toISOString()}
-    </li>
-  `;
-
+  fetch(cacheApi)
+    .then(data => data.json())
+    .then(cache => {
+      resultsView.innerHTML += `
+        <li>
+          User: <a href="../">${cache.name}</a>
+        </li>
+        <li>
+          ID: <a href="">${posts[0].id}</a>
+        </li>
+        <li>
+          Published at: ${posts[0].published_at}
+        </li>
+        <li>
+          Added at: ${new Date(posts[0].added_at).toISOString()}
+        </li>
+      `;
+    })
+  
   fetch(flagApi)
     .then(res => {
       resultsView.innerHTML += res.ok ? `

ddaba6792e4f48462e084a5db3d9ebec234ef9c3
Anonymous <>
2020-07-05 21:24:04 -0400

    Delete proxy.js

--- proxy.js
+++
@@ -1,9 +0,0 @@
-const retry = require('p-retry');
-// probably won't work with non-promise request libs
-module.exports = (uri, options, wrap) => {
-  return retry(i => {
-    let proxy = process.env.PROXY ? process.env.PROXY : '';
-    if (i > 1) proxy = '';
-    return wrap.get(proxy + uri, options);
-  });
-};

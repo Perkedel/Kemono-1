bdf876061093484cea5f00b3f04c5bb801a0f242
Anonymous <>
2020-07-07 06:29:09 -0400

    Update download.js

--- download.js
+++ download.js
@@ -28,11 +28,10 @@ module.exports = (opts, requestOpts = {}) => {
             .on('complete', async (res) => {
               // filename guessing
               const extension = await FileType.fromFile(path.join(opts.ddir, tempname));
-              const namedata = opts.name || res.headers['x-amz-meta-original-filename'] || res.headers['content-disposition'] ? cd.parse(res.headers['content-disposition']).parameters.filename : () => {
+              const name = opts.name || res.headers['x-amz-meta-original-filename'] || res.headers['content-disposition'] ? cd.parse(res.headers['content-disposition']).parameters.filename : () => {
                 return `Untitled.${extension.ext || 'Untitled'}`;
               };
-              const [name, ext] = namedata.split('.');
-              const filename = `${slugify(name, { lowercase: false })}.${ext}`;
+              const filename = slugify(name, { lowercase: false });
               // content integrity
               if (res.statusCode !== 200) return reject(new Error(`Status code: ${res.statusCode}`));
               if (res.headers['content-length']) {

a8b3b00e5a9c2b962ba9c884fe298c82be45d4ab
Anonymous <>
2020-07-10 05:41:34 -0400

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -167,7 +167,6 @@ async function main () {
   });
   document.getElementById('search-input').addEventListener('keyup', debounce(() => queryUpdate(150), 350));
   document.getElementById('service-input').addEventListener('change', () => queryUpdate(150));
-  queryUpdate(150);
 }
 
 window.onload = () => main();

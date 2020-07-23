fd1fb8a91bbcb25494e64045dc911fe928ff5a0b
Anonymous <>
2020-07-23 05:49:28 -0400

    header changes
    - "Kemono" button is redundant
    - Move importer to top header

--- views/components/header.js
+++ views/components/header.js
@@ -7,9 +7,9 @@
 
 const header = props => `
   <ul class="header">
-    <li><a href="/">Kemono</a></li>
     <li ${props.currentPage === 'artists' ? 'class="current-page"' : ''}><a href="/artists">Artists</a></li>
     <li ${props.currentPage === 'posts' ? 'class="current-page"' : ''}><a href="/posts">Posts</a></li>
+    <li ${props.currentPage === 'import' ? 'class="current-page"' : ''}><a href="/importer">Import</a></li>
     <li ${props.currentPage === 'board' ? 'class="current-page"' : ''}><a href="/board">Board</a></li>
     <li ${props.currentPage === 'help' ? 'class="current-page"' : ''}><a href="/help">Help</a></li>
     <li><a href="https://liberapay.com/kemono.party" target="_blank">Donate</a></li>
--- views/components/subheader.js
+++ views/components/subheader.js
@@ -9,7 +9,6 @@ const subheader = props => ({
   posts: `
     <ul class="subheader">
       <li><a href="/posts">List</a></li>
-      <li><a href="/importer">Import</a></li>
       <li><a href="/random">Random</a></li>
       <li><a href="/help/posts">Help</a></li>
     </ul>

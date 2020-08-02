9867d1dae9bdd530437d5745217d3dfe44e8560e
Anonymous <>
2020-08-02 01:07:18 -0400

    Support negative queries in query builder

--- querybuilder.js
+++ querybuilder.js
@@ -4,9 +4,15 @@ module.exports = (str) => {
   let query = {}; // mongodb query
   const tags = str.replace(/\s\s+/g, ' ').trim().split(' ');
   for (var i = 0; i < tags.length; i++) {
-    // TODO: Add negative queries
     // general tags
-    if (!/:/.test(tags[i])) {
+    if (!/:/.test(tags[i]) && tags[i].startsWith('-')) {
+      query = merge(query, {
+        'tags.general': {
+          $nin: [tags[i].endsWith('*') ? { $regex: '^' + esc(tags[i].slice(0, -1).replace(/_/, ' ').replace('-', '')) } : tags[i].replace(/_/, ' ').replace('-', '')]
+        }
+      });
+      continue;
+    } else if (!/:/.test(tags[i])) {
       query = merge(query, {
         'tags.general': {
           $all: [tags[i].endsWith('*') ? { $regex: '^' + esc(tags[i].slice(0, -1).replace(/_/, ' ')) } : tags[i].replace(/_/, ' ')]
@@ -19,16 +25,28 @@ module.exports = (str) => {
     const [namespace, pretag] = tags[i].split(':');
     const tag = pretag.endsWith('*') ? { $regex: '^' + esc(pretag.slice(0, -1).replace(/_/, ' ')) } : pretag.replace(/_/, ' ')
     const metatags = ['id', 'user', 'service', 'added_at', 'published_at', 'edited_at'];
-    if (metatags.includes(namespace)) {
+    if (metatags.includes(namespace.replace('-', '')) && namespace.startsWith('-')) {
+      query[namespace.replace('-', '')] = { $ne: tag };
+      continue;
+    } else if (metatags.includes(namespace)) {
       query[namespace] = tag;
       continue;
     }
 
-    query = merge(query, {
-      ['tags.' + namespace]: {
-        $all: [tag]
-      }
-    });
+    if (namespace.startsWith('-')) {
+      query = merge(query, {
+        ['tags.' + namespace.replace('-', '')]: {
+          $nin: [tag]
+        }
+      });
+    } else {
+      query = merge(query, {
+        ['tags.' + namespace]: {
+          $all: [tag]
+        }
+      });
+    }
+    
   }
   return query;
 };

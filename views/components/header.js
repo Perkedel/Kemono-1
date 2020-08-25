25e7417cb8550df2a8cfeec6be7495dd9f7c531a
Anonymous <>
2020-08-25 08:30:37 -0400

    Revert "Update header.js"
    This reverts commit 9940240b6aad917b5422c147d5afe2aa1d5f2c49.

--- views/components/header.js
+++ views/components/header.js
@@ -12,6 +12,7 @@ const header = props => `
     <li ${props.currentPage === 'import' ? 'class="current-page"' : ''}><a href="/importer">Import</a></li>
     <li ${props.currentPage === 'board' ? 'class="current-page"' : ''}><a href="/board">Board</a></li>
     <li ${props.currentPage === 'help' ? 'class="current-page"' : ''}><a href="/help">Help</a></li>
+    <li><a href="https://liberapay.com/kemono.party" target="_blank">Donate</a></li>
   </ul>
 `;
 

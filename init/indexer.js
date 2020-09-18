010e9c430f5512446700bca88f9055271ec234a7
Anonymous <>
2020-09-18 08:59:49 -0400

    Update indexer.js

--- init/indexer.js
+++ init/indexer.js
@@ -2,16 +2,6 @@ const Promise = require('bluebird');
 const request = require('request-promise');
 const retry = require('p-retry');
 const { unraw } = require('unraw');
-// const cloudscraper = require('cloudscraper').defaults({
-//   agentOptions: {
-//     ciphers: [
-//       'TLS_AES_128_GCM_SHA256',
-//       'TLS_CHACHA20_POLY1305_SHA256',
-//       'TLS_AES_256_GCM_SHA384',
-//       'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256'
-//     ].join(':')
-//   }
-// });
 const agentOptions = require('../utils/agent');
 const cloudscraper = require('cloudscraper').defaults({ agentOptions });
 const { db } = require('../utils/db');

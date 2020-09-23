8991609dc0203b147bd373cab09cd808c5c42b51
Anonymous <>
2020-09-23 15:30:29 -0400

    Keep logging in separate database pool

--- utils/db.js
+++ utils/db.js
@@ -14,6 +14,20 @@ module.exports = {
     },
     acquireConnectionTimeout: 1000000
   }),
+  logdb: require('knex')({
+    client: 'pg',
+    connection: {
+      host: process.env.PGHOST,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE
+    },
+    pool: {
+      acquireTimeoutMillis: 1000000, // never timeout
+      max: 1
+    },
+    acquireConnectionTimeout: 1000000
+  }),
   cache: new Redis({
     host: process.env.RDHOST || 'localhost',
     port: process.env.RDPORT || 6379
--- utils/debug.js
+++ utils/debug.js
@@ -1,8 +1,8 @@
 const debug = require('debug');
-const { db } = require('./db');
+const { logdb } = require('./db');
 debug.log = (...args) => {
   process.stdout.write(args.map(x => x.trim()).join(' ') + '\n');
-  db('logs').insert({
+  logdb('logs').insert({
     log0: args[0].replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '').trim(),
     log: args.map(x => x.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '').trim())
   }).asCallback(() => {});

1bad5ea1376c9e48a1ec7cb6de5ea5d4b2787348
Anonymous <>
2020-09-24 18:43:49 -0400

    Revert "Switch to Adsterra"
    This reverts commit 9ec3189c8785351e22d0907320244599aa80240f.

--- views/components/list.js
+++ views/components/list.js
@@ -3,16 +3,9 @@ const { paginator } = require('./paginator');
 
 const list = props => `
   <div class="vertical-views">
-    <script type="text/javascript">
-	    atOptions = {
-	    	'key' : '89174c8852867044f295023fb5f0015f',
-	    	'format' : 'iframe',
-	    	'height' : 60,
-	    	'width' : 468,
-	    	'params' : {}
-	    };
-	    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.displaycontentnetwork.com/89174c8852867044f295023fb5f0015f/invoke.js"></scr' + 'ipt>');
-    </script>
+    <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+    <ins id="871536" data-width="468" data-height="72"></ins>
+    <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':871536});</script>
     <div class="paginator" id="paginator-top">
       ${paginator({
         o: props.o,
@@ -38,16 +31,9 @@ const list = props => `
         count: props.count
       })}
     </div>
-    <script type="text/javascript">
-	    atOptions = {
-	    	'key' : '89174c8852867044f295023fb5f0015f',
-	    	'format' : 'iframe',
-	    	'height' : 60,
-	    	'width' : 468,
-	    	'params' : {}
-	    };
-	    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.displaycontentnetwork.com/89174c8852867044f295023fb5f0015f/invoke.js"></scr' + 'ipt>');
-    </script>
+    <script type="text/javascript" data-cfasync="false" async src="https://poweredby.jads.co/js/jads.js"></script>
+    <ins id="871536" data-width="468" data-height="72"></ins>
+    <script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({'adzone':871536});</script>
   </div> 
 `;
 

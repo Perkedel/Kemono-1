d06561a76dad59fb1bceca1a251716dfbfb03226
Anonymous <>
2020-09-25 04:43:33 -0400

    Update builders.js

--- utils/builders.js
+++ utils/builders.js
@@ -10,13 +10,12 @@ const splitWithoutTruncation = (string, separator, n) => {
 const booruQueryFromString = str => {
   let query = db('booru_posts');
   let tags = str.replace(/\s\s+/g, ' ').trim().split(' ');
+  let regtags = [];
   let where = [];
   let whereBindings = [];
   console.log(tags)
   tags.map(tag => {
-    if (!/:/.test(tag)) return console.log('aaaa ' + tag);
-    tags.splice(tags.indexOf(tag), 1);
-    console.log(tags)
+    if (!/:/.test(tag)) return regtags.push(tag);
     const [namespace, nametag] = splitWithoutTruncation(tag, ':', 2);
     const metatags = [
       'id',
@@ -27,15 +26,15 @@ const booruQueryFromString = str => {
       'edited',
       'rating'
     ];
-    if (!metatags.includes(namespace.replace('-', ''))) return console.log('aaaa ' + tag);
+    if (!metatags.includes(namespace.replace('-', ''))) return;
     let not = namespace.startsWith('-');
     let wildcard = nametag.endsWith('*');
     if (not && wildcard) {
       where.push(`NOT ${namespace.replace('-', '')} ILIKE '?'`);
-      whereBindings.push(nametag + '%')
+      whereBindings.push(nametag.replace('*', '%'))
     } else if (!not && wildcard) {
       where.push(`${namespace.replace('-', '')} ILIKE '?'`);
-      whereBindings.push(nametag + '%')
+      whereBindings.push(nametag.replace('*', '%'))
     } else if (not && !wildcard) {
       where.push(`NOT ${namespace.replace('-', '')} = '?'`);
       whereBindings.push(nametag);
@@ -44,8 +43,8 @@ const booruQueryFromString = str => {
       whereBindings.push(nametag);
     }
   });
-  query.whereRaw(where.join(' AND '), whereBindings);
-  if (tags.length) query.andWhereRaw('to_tsvector(tags) @@ websearch_to_tsquery(?)', [tags.join(' ')])
+  query.whereRaw(where.join(' and '), whereBindings);
+  if (regtags.length) query.andWhereRaw('to_tsvector(tags) @@ websearch_to_tsquery(?)', [regtags.join(' ')])
   return query;
 }
 

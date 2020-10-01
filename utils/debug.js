ae9676cba56b0416940b875786df87f3c876a4bc
Anonymous <>
2020-10-01 11:52:34 -0400

    Revert "Update debug.js"
    This reverts commit de788e6f97bd8d57b48b986e84e2cfe14fda31d9.

--- utils/debug.js
+++ utils/debug.js
@@ -1,7 +1,7 @@
 const debug = require('debug');
 const { logdb } = require('./db');
 debug.log = (...args) => {
-  process.stdout.write(args.join(' ') + '\n');
+  process.stdout.write(args.map(x => x.trim()).join(' ') + '\n');
   /* eslint-disable no-control-regex */
   logdb('logs').insert({
     log0: args[0].replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '').trim(),

9a4412014b1cdd1dd4d81ac3c6eb8f8f0f78feed
Anonymous <>
2020-10-01 08:28:49 -0400

    Use log pool for retrieval

--- routes/importer/index.js
+++ routes/importer/index.js
@@ -1,5 +1,5 @@
 const { list, ok, tutorial, yiff, status } = require('./views');
-const { db } = require('../../utils/db');
+const { logdb } = require('../../utils/db');
 const express = require('express');
 const router = express.Router();
 
@@ -9,7 +9,7 @@ router
   .get('/ok', (_, res) => res.set('Cache-Control', 'max-age=60, public, stale-while-revalidate=2592000').send(ok()))
   .get('/yiff', (_, res) => res.set('Cache-Control', 'max-age=60, public, stale-while-revalidate=2592000').send(yiff()))
   .get('/status/:id', async (req, res) => {
-    const logs = await db('logs')
+    const logs = await logdb('logs')
       .where('log0', 'ILIKE', '%' + ':' + req.params.id + '%')
       .orderBy('created', 'asc');
     res.set('Cache-Control', 's-maxage=31557600, no-cache');

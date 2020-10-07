1e5f18a26a7712af02a752fc975741707eaed0b9
Anonymous <>
2020-10-07 17:13:58 -0400

    Update debug.js

--- utils/debug.js
+++ utils/debug.js
@@ -1,10 +1,11 @@
 const debug = require('debug');
+const util = require('util');
 const { logdb } = require('./db');
 
 debug.dlog = debug.log;
 debug.log = (...args) => {
   try {
-    process.stdout.write(args.map(x => x.trim()).join(' ') + '\n');
+    process.stdout.write(util.format(...args) + '\n');
     /* eslint-disable no-control-regex */
     logdb('logs').insert({
       log0: args[0].replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '').trim(),

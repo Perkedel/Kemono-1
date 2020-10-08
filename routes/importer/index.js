b28f2c158034488e8e9822d17d520066a88318c4
Anonymous <>
2020-10-07 21:01:43 -0400

    Update index.js

--- routes/importer/index.js
+++ routes/importer/index.js
@@ -10,7 +10,6 @@ router
   .get('/yiff', (_, res) => res.set('Cache-Control', 'max-age=60, public, stale-while-revalidate=2592000').send(yiff()))
   .get('/status/:id', async (req, res) => {
     const logs = await logdb('logs').whereRaw('to_tsvector(\'english\', log0) @@ websearch_to_tsquery(?)', ['kemono:importer:status:' + req.params.id]);
-    console.log(logs)
     res.set('Cache-Control', 's-maxage=31557600, no-cache');
     res.send(status({
       id: req.params.id,

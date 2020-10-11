a28824c6803b5c08349b5b1a21778119c850f76d
Anonymous <>
2020-10-11 11:08:46 -0400

    Revert "Use Graphicsmagick for thumbs"

--- init/server.js
+++ init/server.js
@@ -6,7 +6,7 @@ const imageType = require('image-type');
 const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs-extra');
-const gm = require('gm');
+const sharp = require('sharp');
 const { db, cache } = require('../utils/db');
 const path = require('path');
 const Promise = require('bluebird');
@@ -58,18 +58,25 @@ module.exports = () => {
       const fileExists = await fs.pathExists(file);
       if (!fileExists) return res.sendStatus(404);
       if (process.env.DISABLE_THUMBNAILS === 'true') return fs.createReadStream(file).pipe(res);
-      
-      res.set('Cache-Control', 'max-age=31557600, public');
-      gm(file).identify(function (err, _) {
-        if (err) return res.status(404).send('Not an image.');
-        gm(file)
-          .resize(Number(req.query.size) && Number(req.query.size) <= 800 ? Number(req.query.size) : 800, null, '>')
-          .quality(60)
-          .interlace('Line')
-          .noProfile()
-          .stream('jpg')
-          .pipe(res);
-      });
+      const type = imageType(await readChunk(file, 0, imageType.minimumBytes));
+      let ext = type ? type.ext : '';
+      ext = ext === 'jpg' ? 'jpeg' : ext;
+      const fileSupported = sharp.format[ext] ? sharp.format[ext].input.file : false;
+      if (!fileSupported) return res.sendStatus(404);
+      res.setHeader('Cache-Control', 'max-age=31557600, public');
+      sharp(file, { failOnError: false })
+        .jpeg({
+          quality: 60,
+          chromaSubsampling: '4:2:0',
+          progressive: true
+        })
+        .resize({ width: Number(req.query.size) && Number(req.query.size) <= 800 ? Number(req.query.size) : 800, withoutEnlargement: true })
+        .setMaxListeners(250)
+        .on('error', () => {
+          fs.createReadStream(file)
+            .pipe(res);
+        })
+        .pipe(res);
     })
     .use(cacheMiddleware())
     .get('/', async (req, res) => {
